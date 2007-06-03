9e9c4df9f7bfd78cc6cd460b5f30681abde92d92
Andrew Arnott <AndrewArnott@gmail.com>
2007-06-03 05:51:00 +0000

    Adding, deleting, and changing clues now supported via data binding, although disproving players is still not correctly mapped.
    darcs-hash:20070603055111-79701-cb2a31e3be42c688b0d464004003ba4508c643c0.gz

--- ClueBuddyGui/CompositeClueBrowser.xaml.cs
+++ ClueBuddyGui/CompositeClueBrowser.xaml.cs
@@ -21,12 +21,55 @@ namespace ClueBuddyGui {
 	public partial class CompositeClueBrowser : System.Windows.Controls.UserControl {
 		public CompositeClueBrowser() {
 			InitializeComponent();
-			//ComboBox cb = (ComboBox)cc.FindName("suggester");
-			//cb.ItemsSource = ((Game)DataContext).Players;
-			//cb.ItemsSource = Game.GreatDetective.Players;
+		}
+
+		Game game {
+			get { return DataContext as Game; }
+		}
+		CollectionViewSource cluesDataView {
+			get { return Resources["cluesDataView"] as CollectionViewSource; }
+		}
+		CollectionViewSource opponentsDataView {
+			get { return Resources["opponentsDataView"] as CollectionViewSource; }
+		}
+		Clue currentClue {
+			get { return cluesDataView.View.CurrentItem as Clue; }
+		}
+
+		Player selectingPlayer;
+		void suggestingPlayer_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			selectingPlayer = e.AddedItems.Count > 0 ? e.AddedItems[0] as Player : null;
+			opponentsDataView.View.Refresh();
+			selectingPlayer = null;
+		}
+
+		void filterSelectedPlayer(object sender, FilterEventArgs e) {
+			if (currentClue != null) {
+				Player p = (Player)e.Item;
+				Player suggestingPlayer = selectingPlayer ?? currentClue.Player;
+				e.Accepted = p != suggestingPlayer;
+			} else {
+				e.Accepted = true;
+			}
 		}
 
 		void previousClueButton_Click(object sender, EventArgs e) {
+			if (!cluesDataView.View.MoveCurrentToPrevious())
+				cluesDataView.View.MoveCurrentToFirst();
+		}
+
+		void nextClueButton_Click(object sender, EventArgs e) {
+			if (!cluesDataView.View.MoveCurrentToNext())
+				cluesDataView.View.MoveCurrentToLast();
+		}
+
+		void newClueButton_Click(object sender, EventArgs e) {
+			game.Clues.Add(new CompositeClue());
+			cluesDataView.View.MoveCurrentToLast();
+		}
+
+		void deleteClueButton_Click(object sender, EventArgs e) {
+			game.Clues.Remove(currentClue);
 		}
 	}
 }
